c97e7e415ad76009194fb064da61003746103dd6
Joseph Konop <joseph@netengine.com.au>
2018-11-08 21:23:45 +1000

    installing rspec

---
+++ spec/requests/v1/image_resizer_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Image Resizer', type: :request do
+  describe "#create" do
+    context "when valid params" do
+        # post '/api/v1/resize', params: {
+        #   user_ids: [attendees.first.provider_id, invalid_id],
+        #   subject: email_subject,
+        #   message: message
+        # }
+      before do
+        post '/api/v1/resize'
+      end
+
+      it "response" do
+        expect(response.body).to  eq('')
+      end
+    end
+
+  end
+end
