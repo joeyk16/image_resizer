4d5acfbba55676a4926c1a06040fb54549eabfc7
Joseph Konop <joseph@netengine.com.au>
2018-11-13 21:52:43 +1000

    image resizer specs

--- spec/requests/v1/image_resizer_spec.rb
+++ spec/requests/v1/image_resizer_spec.rb
@@ -3,19 +3,52 @@
 require 'rails_helper'
 
 RSpec.describe 'Image Resizer', type: :request do
+  subject(:res_body) { JSON.parse(response.body, symbolize_names: true) }
+
   describe '#create' do
     context 'when valid params' do
-      # post '/api/v1/resize', params: {
-      #   user_ids: [attendees.first.provider_id, invalid_id],
-      #   subject: email_subject,
-      #   message: message
-      # }
+      let(:valid_params) do
+        {
+          image: {
+            url: ActionController::Base.helpers.asset_url('cat.jpg'),
+            width: 300,
+            height: 200
+          }
+        }
+      end
+
       before do
-        post '/api/v1/resize'
+        allow_any_instance_of(MiniMagick::Image).to receive(:open).and_return(nil)
       end
 
       it 'response' do
-        expect(response.body).to eq('')
+        expect do
+          post '/api/v1/resize', params: valid_params
+        end.to change { Image.count }.by(1)
+
+        expect(response.body).to include('http://localhost:3000/rails/active_storage/blobs/')
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'when invalid params' do
+      let(:invalid_params) do
+        {
+          image: {
+            url: 'www.invalid_url.com/image.jpg'
+          }
+        }
+      end
+
+      it 'response' do
+        expect do
+          post '/api/v1/resize', params: invalid_params
+        end.to change { Image.count }.by(0)
+
+        expect(res_body).to eq(
+          errors: 'No such file or directory @ rb_sysopen - www.invalid_url.com/image.jpg'
+        )
+        expect(response).to have_http_status(:bad_request)
       end
     end
   end
